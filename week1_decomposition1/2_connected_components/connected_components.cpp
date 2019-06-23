0856dec48ca6dd0e90683814bed556b1f3e5c8ef
Saruar Manarbek <sar.manar@gmail.com>
2019-06-23 04:43:46 -0400

    solved week 1 problem 2

--- week1_decomposition1/2_connected_components/connected_components.cpp
+++ week1_decomposition1/2_connected_components/connected_components.cpp
@@ -3,22 +3,54 @@
 
 using std::vector;
 using std::pair;
+using namespace std;
 
-int number_of_components(vector<vector<int> > &adj) {
-  int res = 0;
-  //write your code here
-  return res;
+
+// Find all nodes reachable from a given one
+void exploreNode(vector<vector<int> > &adj, vector<bool> &visited, vector<int> &ccnum, int &compNum, int node) {
+    visited[node] = true;
+    ccnum[node] = compNum;
+    for (int i=0; i<adj[node].size(); i++){
+        if (!visited[adj[node][i]])
+            exploreNode(adj, visited, ccnum, compNum, adj[node][i]);
+    }
 }
+
+
+// Number all the interlinked components in the graph and return the number of components
+int dfs(vector<vector<int> > &adj, vector<bool> &visited, vector<int> &ccnum) {
+    int compNum = 1;
+    for (int i=0; i<adj.size(); i++){
+        if (!visited[i]){
+            exploreNode(adj, visited, ccnum, compNum, i);
+            compNum++;
+        }
+    }
+    return compNum-1;
+}
+
 
 int main() {
   size_t n, m;
   std::cin >> n >> m;
+
+  // Adjacency list
   vector<vector<int> > adj(n, vector<int>());
+
+  // Auxilary vector to track visited nodes
+  vector<bool> visited (n, false);
+
+  // Auxilary vector to track the connected component number all initialized to -1
+  vector<int> ccnum (n, -1);
+
+  // Populate the adjacency list
   for (size_t i = 0; i < m; i++) {
     int x, y;
     std::cin >> x >> y;
     adj[x - 1].push_back(y - 1);
     adj[y - 1].push_back(x - 1);
   }
-  std::cout << number_of_components(adj);
+
+  //std::cout << number_of_components(adj);
+  cout << dfs(adj, visited, ccnum);
 }
