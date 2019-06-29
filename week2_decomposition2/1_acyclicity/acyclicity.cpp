0928fe80e2bc0e9ffbb5c0de7ec0eb685568f433
Saruar Manarbek <sar.manar@gmail.com>
2019-06-29 18:18:18 -0400

    solved week 2 problem 1

--- week2_decomposition2/1_acyclicity/acyclicity.cpp
+++ week2_decomposition2/1_acyclicity/acyclicity.cpp
@@ -1,12 +1,7 @@
 #include <iostream>
 #include <vector>
 #include <stack>
-#include <unordered_map>
-#include <queue>
 using namespace std;
-
-using std::vector;
-using std::pair;
 
 
 // These are the values with which we populate the flag vector
@@ -15,7 +10,7 @@ const int VISITED = 0;
 const int VISITED_POPPED = 1;
 
 
-// Needs improvement. Currently runs in O(|V|*|E|). We need O(|V|+|E|)
+
 int hasCycle(vector<vector<int> > &adj){
     int n = adj.size();
 
@@ -31,17 +26,18 @@ int hasCycle(vector<vector<int> > &adj){
     s.push(0);
     visitedNodes[0] = true;
     flag[0] = VISITED;
-    cout << 0 << " ";
+    //cout << 0 << " ";
 
     while(!s.empty()){
         int p = s.top();
 
-        // Check any adjacent nodes with flag[i] = VISITED
+        // Check any adjacent nodes with flag[i] = VISITED; if yes then we have a cycle
         for (int i=0; i<adj[p].size(); i++){
             if (flag[adj[p][i]] == VISITED)
                 return 1;
         }
 
+        // Find an unvisited node an push into the stack
         bool hasAdjVertex = false;
         for (int i=0; i<adj[p].size(); i++){
             if (!visitedNodes[adj[p][i]]){
@@ -49,7 +45,7 @@ int hasCycle(vector<vector<int> > &adj){
                 visitedNodes[adj[p][i]] = true;
                 flag[adj[p][i]] = VISITED;
                 hasAdjVertex = true;
-                cout << adj[p][i] << " ";
+                //cout << adj[p][i] << " ";
                 break;
             }
         }
@@ -59,7 +55,7 @@ int hasCycle(vector<vector<int> > &adj){
             s.pop();
         }
     }
-    cout << endl;
+    //cout << endl;
     return 0;
 }
 
@@ -74,6 +70,5 @@ int main() {
     std::cin >> x >> y;
     adj[x - 1].push_back(y - 1);
   }
-//  std::cout << acyclic(adj);
-  cout << "ans :" << hasCycle(adj) << endl;
+  std::cout << hasCycle(adj);
 }
