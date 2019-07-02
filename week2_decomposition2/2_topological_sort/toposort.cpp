822312527998990d2148f8bcc1106bf4dcd7cafa
Saruar Manarbek <sar.manar@gmail.com>
2019-07-02 18:39:57 -0400

    solved week 2 problem 3

--- week2_decomposition2/2_topological_sort/toposort.cpp
+++ week2_decomposition2/2_topological_sort/toposort.cpp
@@ -15,55 +15,55 @@ const int BLACK = 1;    // black = visited and finished (popped from stack)
 // Note that it has attributes that are not necessary for this assignment
 // e.g. postOrder, preOrder, time.
 // This algorithm is included here just for my own reference. Not used for the solution
-void dfs_util(vector<vector<int> > &adj, vector<int> &preOrder, vector<int> &postOrder, vector<int> &colors,  int &time, int u) {
-    time++;
-    preOrder[u] = time;
+void dfs_util(vector<vector<int> > &g, vector<int> &preOrder, vector<int> &postOrder, vector<int> &colors,  int &clock, int u) {
+    clock++;
+    preOrder[u] = clock;
     colors[u] = GRAY;
 
-    for (int i = 0; i<adj[u].size(); i++){
-        int v = adj[u][i];
+    for (int i = 0; i<g[u].size(); i++){
+        int v = g[u][i];
         if (colors[v] == WHITE)
-            dfs_util(adj, preOrder, postOrder, colors, time, v);
+            dfs_util(g, preOrder, postOrder, colors, clock, v);
     }
     colors[u] = BLACK;
-    time++;
-    postOrder[u] = time;
+    clock++;
+    postOrder[u] = clock;
 }
 
-void dfs(vector<vector<int> > &adj){
-    int n = adj.size();
+void dfs(vector<vector<int> > &g){
+    int n = g.size();
     vector<int> colors(n, WHITE);
     vector<int> preOrder(n, -1);
     vector<int> postOrder(n, -1);
     int time = 0;
     for (int i=0; i<n; i++){
         if (colors[i]==WHITE)
-            dfs_util(adj, preOrder, postOrder, colors, time, i);
+            dfs_util(g, preOrder, postOrder, colors, time, i);
     }
 }
 
 
 // DFS modified to return a topologically sorted vector.
 // The algorithm dfs (written out above) was modified, attributes not needed for this problem removed
-void dfs_visit_topo(vector<vector<int> > &adj, vector<int> &colors, vector<int> &topoOrder, int u) {
+void dfs_visit_topo(vector<vector<int> > &g, vector<int> &colors, vector<int> &topoOrder, int u) {
     colors[u] = GRAY;
-    for (int i = 0; i<adj[u].size(); i++){
-        int v = adj[u][i];
+    for (int i = 0; i<g[u].size(); i++){
+        int v = g[u][i];
         if (colors[v] == WHITE)
-            dfs_visit_topo(adj, colors, topoOrder, v);
+            dfs_visit_topo(g, colors, topoOrder, v);
     }
     colors[u] = BLACK;
     topoOrder.push_back(u);
 }
 
-vector<int> dfs_topo(vector<vector<int> > &adj){
-    int n = adj.size();
+vector<int> dfs_topo(vector<vector<int> > &g){
+    int n = g.size();
     vector<int> colors(n, WHITE);
     vector<int> topoOrder;
 
     for (int i=0; i<n; i++){
         if (colors[i]==WHITE)
-            dfs_visit_topo(adj, colors, topoOrder, i);
+            dfs_visit_topo(g, colors, topoOrder, i);
     }
     reverse(topoOrder.begin(), topoOrder.end());
     return topoOrder;
